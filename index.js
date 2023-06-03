335206f01d72ee90dbf935071c4e40e9f8511942
negi84 <vipin@mindrops.com>
2023-06-03 16:07:24 +0530

    database setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var bodyParser = require("body-parser");
+require('./models/index')
 
 const app = express();
 
---
+++ models/index.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("employeedb", "root", "", {
+  host: "localhost",
+  port : 3325,
+  dialect: "mysql",
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
